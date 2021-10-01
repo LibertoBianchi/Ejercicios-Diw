c3312c731f66a67a8c1b78f26c321cc8933a8461
Liberto <lbianchi@cifpfbmoll.eu>
2021-10-01 20:56:46 +0200

    Organización y reto dwec

---
+++ Dwec/reto1js/reto1js.js
@@ -0,0 +1,9 @@
+
+function suma(x,y){
+    var x = parseInt(document.getElementById("primero").value);
+var y = parseInt(document.getElementById("segundo").value);
+    
+    var resultado =(parseInt(x+y));
+    document.getElementById("resultado").innerHTML(resultado.toString)
+   
+}
